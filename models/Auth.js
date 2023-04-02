eb918f754167d042173d55ec7fdc8b8c12175f49
Nicolás <silvanicolas93@gmail.com>
2023-04-02 22:34:07 +0000

    Subo models
    Signed-off-by: Nicolás <silvanicolas93@gmail.com>

---
+++ models/Auth.js
@@ -0,0 +1,29 @@
+const { Datastore } = require('@google-cloud/datastore');
+const datastore = new Datastore();
+
+class Auth {
+  constructor() {
+    this.kind = 'User';
+  }
+
+  async findByEmail(email) {
+    const query = datastore.createQuery(this.kind).filter('email', '=', email);
+    const [user] = await datastore.runQuery(query);
+    return user;
+  }
+
+  async create(email, password) {
+    const key = datastore.key(this.kind);
+    const entity = {
+      key,
+      data: {
+        email,
+        password
+      }
+    };
+    await datastore.save(entity);
+    return key;
+  }
+}
+
+module.exports = Auth;
---
+++ models/User.js
@@ -0,0 +1,36 @@
+const {Datastore} = require('@google-cloud/datastore');
+const datastore = new Datastore();
+
+class User {
+  constructor(id, email, password) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+
+  async save() {
+    const key = datastore.key(['User']);
+    const entity = {
+      key: key,
+      data: {
+        id: this.id,
+        email: this.email,
+        password: this.password,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt
+      }
+    };
+    await datastore.save(entity);
+    return entity;
+  }
+
+  static async findByEmail(email) {
+    const query = datastore.createQuery('User').filter('email', '=', email);
+    const [users] = await datastore.runQuery(query);
+    return users[0];
+  }
+}
+
+module.exports = User;
