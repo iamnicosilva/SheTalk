66d96aff6c302b0132244219ad03f2f35c30d977
Nicolás <silvanicolas93@gmail.com>
2023-04-02 19:27:42 +0000

    Push primeros archivos
    Signed-off-by: Nicolás <silvanicolas93@gmail.com>

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+const datastore = require('@google-cloud/datastore')();
+
+const app = express();
+
+// Configuración de la aplicación
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// Rutas
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Manejo de errores 404
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+// Manejo de errores
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+// Inicio del servidor
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
